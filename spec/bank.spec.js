2922e98cd9d7c7e4b4201ad2d228632db3561691
Elle Ordona <eto0507@hotmail.co.uk>
2023-01-24 21:18:21 +0000

    setup test 1

--- spec/bank.spec.js
+++ spec/bank.spec.js
@@ -2,3 +2,19 @@
 
 // import Bank from src
 import Bank from '../src/bank.js';
+
+describe('Testing Bank Class', () => {
+	describe('When creating an instance of Bank', () => {
+		//* TEST 1
+		it('should have runningBalance set to zero', () => {
+			// Arrange
+			const testBank = new Bank(); // create instance of Bank
+
+			// Act
+			const result = testBank.getRunningBalance(); // getter as runningBalance is private
+
+			// Assert
+			expect(result).toBe(0);
+		});
+	});
+});
