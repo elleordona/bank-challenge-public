d3002a3842627ee6f4f79b61dc509273122230f9
Elle Ordona <eto0507@hotmail.co.uk>
2023-01-30 20:55:47 +0000

    added specRunner to show code works for criteria

--- src/bankRunner.js
+++ src/bankRunner.js
@@ -1,16 +1,1 @@
-// showing function of Bank
-
-import Bank from './bank.js';
-import Deposit from './deposit.js';
-import Statement from './statement.js';
-import Withdraw from './withdraw.js';
-
-const myBank = new Bank();
-
-myBank.deposit(new Deposit(1000, '10-01-2012'));
-myBank.deposit(new Deposit(2000, '12-01-2012'));
-myBank.withdraw(new Withdraw(500, '14-01-2012'));
-
-// console.log(myBank.getTransactions());
-
-Statement.getStatement(myBank);
+// runner showing function of Bank
