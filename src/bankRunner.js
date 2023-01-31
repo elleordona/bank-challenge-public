6a7b0a140e6f51c085e59c8e45fcfc2d1ed8f6c3
Elle Ordona <eto0507@hotmail.co.uk>
2023-01-31 10:22:37 +0000

    updated bankRunner to test functionality

--- src/bankRunner.js
+++ src/bankRunner.js
@@ -1,1 +1,17 @@
 // runner showing function of Bank
+
+import Bank from './bank.js';
+import Deposit from './deposit.js';
+import Statement from './statement.js';
+import Withdraw from './withdraw.js';
+
+const myBank = new Bank();
+
+myBank.deposit(new Deposit(1000, '10-01-2012'));
+myBank.deposit(new Deposit(2000, '12-01-2012'));
+myBank.withdraw(new Withdraw(500, '14-01-2012'));
+myBank.withdraw(new Withdraw(5000, '19-01-2012'));
+myBank.deposit(new Deposit(1000, '20-01-2012'));
+myBank.deposit(new Deposit(2000, '23-01-2012'));
+
+Statement.getStatement(myBank);
