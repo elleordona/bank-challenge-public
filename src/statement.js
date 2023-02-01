93e291224e0e1c82823d7afbb022a9a446676318
Elle Ordona <eto0507@hotmail.co.uk>
2023-02-01 14:58:05 +0000

    get rid of the bank property in Statement it wasn't needed

--- src/statement.js
+++ src/statement.js
@@ -1,11 +1,9 @@
 export default class Statement {
 	// properties
-	static #bank;
 	static #bankTransactions;
 
 	// methods
 	static getStatement(bank) {
-		this.#bank = bank;
 		this.#bankTransactions = bank.getTransactions();
 		return console.table(this.#bankTransactions, ['date', 'credit', 'debit', 'balance']);
 	}
